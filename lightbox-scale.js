28c2521ef4d66c25b8ccf6b81d5235192fd4dba9
Nathanael McMillan <nmcmillan@perfectcolours.com>
2014-11-12 16:50:32 +0000

    Create lightbox on if image is scaled

---
+++ lightbox-scale.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+/**
+* Easy Lightboxish
+* This checks all image on the page, if the images 
+* natural width is larger than its parent width it
+* adds a lightbox wrapper to the img.
+*/
+$("img.scaleBox").each(function() {
+    var image = $(this);
+    var imageUrl = $(this).attr('src');
+    var imageAlt = $(this).attr('alt');
+    var parentEle = $(this).parent();
+    var parentWidth = parentEle.width();
+
+    var buildImage = '<img src="' + imageUrl + '" alt="' + imageAlt + '" class="scaleBox" />';
+    var buildLinkandImage = '<a class="fancybox" href="' + imageUrl + '">' + buildImage + "</a>";
+
+    /**
+     * Attribution: Gabriel McAdams
+     */
+    function getImgSize(imgSrc) {
+        var newImg = new Image();
+
+        newImg.onload = function() {
+            var imgWidth = newImg.width;
+
+            if (imgWidth > parentWidth) {
+                $(image).replaceWith(buildLinkandImage);
+            }
+        }
+
+        newImg.src = imgSrc; // this must be done AFTER setting onload
+    }
+
+    getImgSize(imageUrl);
+
+
+});
